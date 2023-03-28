7d3e041be4b3c1beb1bd68a11377579d26c6df29
dphinojos <dphinojos@arizona.edu>
2023-03-28 19:06:19 +0200

    Still trying to figure this algorithm stuff out

--- TetrisAIProject/TetrisAIProject.py
+++ TetrisAIProject/TetrisAIProject.py
@@ -1,16 +1,50 @@
-
 from nes_py.wrappers import JoypadSpace
 import gym_tetris
 from gym_tetris.actions import MOVEMENT
+import numpy as np
+from problem import *
+from node import *
+from genetic_algorithm import *
 
-env = gym_tetris.make('TetrisA-v0')
+class agent:
+    pass
+
+class Tetris(Problem):
+    def __init__(self, initial, goal=None):
+        super().__init__(initial, goal=None)
+
+    def actions(self, state):
+        allowed_actions = []
+
+        #List of Actions into a numbered array
+        for action in range(len(MOVEMENT)):
+            allowed_actions.append(action)
+
+        return allowed_actions
+    
+    def result(self, state, action):
+
+
+        return action
+
+    def value(self, state):
+        
+        pass
+
+
+
+env = gym_tetris.make('TetrisA-v2')
 env = JoypadSpace(env, MOVEMENT)
+
+TetrisAI = Tetris(env)
+
+TetrisPopulation = genetic_search(TetrisAI)
 
 done = True
 for step in range(5000):
     if done:
         state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
+    state, reward, done, info = env.step(0)
     env.render()
 
 env.close()
